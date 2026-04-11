cf47dcc17b39a7a741f56b12768bc6b0e50bb843
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2026-04-10 21:34:01 -0400

    Remove wallet send per-IP limiter

--- api_handlers.go
+++ api_handlers.go
@@ -1103,12 +1103,6 @@ func (s *APIServer) handleSend(w http.ResponseWriter, r *http.Request) {
 		}()
 	}
 
-	ip := clientIP(r)
-	if !s.sendLimiter.allow(ip) {
-		writeError(w, http.StatusTooManyRequests, "send rate limit exceeded")
-		return
-	}
-
 	select {
 	case s.sendSem <- struct{}{}:
 		defer func() { <-s.sendSem }()
@@ -1298,12 +1292,6 @@ func (s *APIServer) handleSendAdvanced(w http.ResponseWriter, r *http.Request) {
 				s.sendIdem.abandon(cacheKey)
 			}
 		}()
-	}
-
-	ip := clientIP(r)
-	if !s.sendLimiter.allow(ip) {
-		writeError(w, http.StatusTooManyRequests, "send rate limit exceeded")
-		return
 	}
 
 	select {
