blocknetprivacy/core

Prevent wallet scans from following forked blocks

cli_cmd_wallet.go
@@ -711,6 +711,12 @@ func memoTextIfPrintable(b []byte) (string, bool) {
 }
 
 func (c *CLI) cmdSync() {
+	if removed := rewindWalletToCanonicalTip(c.wallet, c.daemon.Chain()); removed > 0 {
+		fmt.Printf("  Chain reset: removed %d orphaned outputs, rewound to height %d\n", removed, c.wallet.SyncedHeight())
+		if err := c.wallet.Save(); err != nil {
+			fmt.Printf("  Warning: failed to persist rewound wallet: %v\n", err)
+		}
+	}
 	chainHeight := c.daemon.Chain().Height()
 	walletHeight := c.wallet.SyncedHeight()
 
@@ -729,6 +735,7 @@ func (c *CLI) cmdSync() {
 	blocks := c.daemon.Chain().GetBlocksByHeightRange(walletHeight+1, chainHeight)
 
 	scannedTo := walletHeight
+	var scannedHash [32]byte
 	for _, block := range blocks {
 		if block == nil {
 			break
@@ -739,13 +746,14 @@ func (c *CLI) cmdSync() {
 
 		h := block.Header.Height
 		scannedTo = h
+		scannedHash = blockData.Hash
 		if found > 0 || spent > 0 {
 			fmt.Printf("    Block %d: +%d outputs, %d spent\n", h, found, spent)
 		}
 	}
 
 	if scannedTo > walletHeight {
-		c.wallet.SetSyncedHeight(scannedTo)
+		c.wallet.SetSyncedBlock(scannedTo, scannedHash)
 		fmt.Printf("  Wallet synced to height %d\n", scannedTo)
 	}
 }

zpalmtree/blocknet-core

cli_cmd_wallet.go
@@ -741,15 +741,17 @@ func (c *CLI) cmdSync() {
 	// Scan the whole batch with one spendable-key-image index (built once,
 	// maintained incrementally) instead of rebuilding it per block.
 	scannedTo := walletHeight
-	c.scanner.ScanBlocksReport(scanBlocks, func(h uint64, found, spent int) {
-		scannedTo = h
+	var scannedHash [32]byte
+	c.scanner.ScanBlocksReport(scanBlocks, func(block *wallet.BlockData, found, spent int) {
+		scannedTo = block.Height
+		scannedHash = block.Hash
 		if found > 0 || spent > 0 {
-			fmt.Printf("    Block %d: +%d outputs, %d spent\n", h, found, spent)
+			fmt.Printf("    Block %d: +%d outputs, %d spent\n", block.Height, found, spent)
 		}
 	})
 
 	if scannedTo > walletHeight {
-		c.wallet.SetSyncedHeight(scannedTo)
+		c.wallet.SetSyncedBlock(scannedTo, scannedHash)
 		fmt.Printf("  Wallet synced to height %d\n", scannedTo)
 	}
 }